cdc1026c0108bcf4bfc9ada35af420c6d4a93f99
amdaza <amdazacastillo@gmail.com>
2016-10-30 18:37:41 +0100

    Create Xcode project

---
+++ ScoopClient/ScoopClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScoopClient
+//
+//  Created by Home on 30/10/16.
+//  Copyright © 2016 Alicia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
